99d4f6186630facae9ff14d0916c6675d85bc14a
Andrew Nisbet <andrew@dev-ils.com>
2023-06-16 11:32:18 -0400

    Now handles multiple scripts, both as daemon and one-off scripts. New --daemon switch replaces --bg. Commands file now required as are email addresses.

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
 # monstat
-Starts and monitors status of arbitrary scripts.
+Starts and monitors status of an arbitrary script.
+
+# Files
+- `commands` - Contains the commands you wish to check running then start. Blank lines and lines that start with `#` are ignored.
+- `monstat.log` - Ongoing log file of events.
+- `monstat.notified` - Keeps track of the last time staff were notified, and if the service failed and was not emailed today, then an email will be sent. Read; an email about a failed service will be sent once a day for every day of failure.
+- `monstat.tries` - Keeps track of the number of times attempts were made to start the service. If the service was started successfully, the count is zero-ed out.
+- `README.md` - This file.
+- `monstat.sh` - Application script.
+- `LICENSE` - Nuff said.
+- `test.sh` - Simple background-able script to test various features.
+
+# Use cases
+## Starts Background Service Successfully
+`monstat` keeps track of two long running processes: `./monstat.sh --retry=4 --daemon --commands=./commands` where `commands` contains `/home/anisbet/Dev/EPL/monstat/test.sh 5` and `/home/anisbet/Dev/EPL/monstat/test2.sh 15`.
+
+```bash
+[2023-06-16 10:35:47] Starting '/home/anisbet/Dev/EPL/monstat/test.sh 5' daemon, attempt no. 1
+[2023-06-16 10:35:47] Starting '/home/anisbet/Dev/EPL/monstat/test2.sh 15' daemon, attempt no. 1
+sleep time of 5 seconds is over
+[2023-06-16 10:35:52] Starting '/home/anisbet/Dev/EPL/monstat/test.sh 5' daemon, attempt no. 1
+sleep time of 5 seconds is over
+[2023-06-16 10:35:58] Starting '/home/anisbet/Dev/EPL/monstat/test.sh 5' daemon, attempt no. 1
+sleep time of 15 seconds is over
+[2023-06-16 10:36:03] Starting '/home/anisbet/Dev/EPL/monstat/test2.sh 15' daemon, attempt no. 1
+sleep time of 5 seconds is over
+[2023-06-16 10:36:04] Starting '/home/anisbet/Dev/EPL/monstat/test.sh 5' daemon, attempt no. 1
+sleep time of 5 seconds is over
+sleep time of 15 seconds is over
+```
+
+## Start Two Daemon Services But One Fails
+Application keeps track of two long running processes: `./monstat.sh --retry=4 --daemon --commands=./commands` where `commands` contains `/home/anisbet/Dev/EPL/monstat/test.sh 5` and `/home/anisbet/Dev/EPL/monstat/test2.sh 15`, but `test2.sh` does not exist.
+```bash
+[2023-06-16 10:51:59] Starting '/home/anisbet/Dev/EPL/monstat/test.sh 5' daemon, attempt no. 1
+[2023-06-16 10:51:59] Starting '/home/anisbet/Dev/EPL/monstat/test2.sh 15' daemon, attempt no. 1
+[2023-06-16 10:52:00] *warning, attempt # 2 to start /home/anisbet/Dev/EPL/monstat/test2.sh 15 as daemon
+[2023-06-16 10:52:01] *warning, attempt # 3 to start /home/anisbet/Dev/EPL/monstat/test2.sh 15 as daemon
+[2023-06-16 10:52:02] *warning, attempt # 4 to start /home/anisbet/Dev/EPL/monstat/test2.sh 15 as daemon
+[2023-06-16 10:52:03] **error, test2.sh daemon failed to start after 4 attempts
+[2023-06-16 10:52:03] someone@example.com emailed about test2.sh failure on Fri 16 Jun 2023 10:52:03 AM EDT
+[2023-06-16 10:52:03] **Attention, as of Fri 16 Jun 2023 10:52:03 AM EDT application test2.sh is NOT running and failed to restart. 
+Please investigate.
+
+[2023-06-16 10:52:03] *error, no mail service !
+sleep time of 5 seconds is over
+```
+
+## Starts Two Non-Daemon Scripts Successfully
+Run from the command line as `./monstat.sh --retry=4 --commands=./commands2` with commands of `ls -a` and `echo "hello world"` which produces the following.
+```bash
+[2023-06-16 10:59:18] command 'ls -a' output:
+.
+..
+commands
+ (continues...)
+test.sh
+[2023-06-16 10:59:18] command 'echo "hello world"' output:
+"hello world"
+```
+
+## Fails to Start Foreground Service
+Run from the command line as `./monstat.sh --retry=4 --commands=./commands2` with commands of `ls -a` and `smello "hello world"` which produces the following.
+```bash
+[2023-06-16 11:03:07] command 'ls -a' output:
+.
+..
+commands
+ (continues...)
+test.sh
+[2023-06-16 11:03:07] command 'smello "hello world"' failed.
+[2023-06-16 11:03:10] command 'ls -a' output:
+.
+..
+commands
+ (continues...)
+test.sh
+[2023-06-16 11:03:10] command 'smello "hello world"' failed.
+[2023-06-16 11:03:33] command 'ls -a' output:
+.
+..
+commands
+ (continues...)
+test.sh
+[2023-06-16 11:03:33] command 'smello "hello world"' failed.
+[2023-06-16 11:03:34] command 'ls -a' output:
+.
+..
+commands
+ (continues...)
+test.sh
+[2023-06-16 11:03:34] command 'smello "hello world"' failed.
+[2023-06-16 11:03:35] command 'ls -a' output:
+.
+..
+commands
+ (continues...)
+test.sh
+[2023-06-16 11:03:35] command 'smello "hello world"' failed.
+[2023-06-16 11:03:35] **error, smello failed to run after 4 attempts
+[2023-06-16 11:03:35] someone@example.com emailed about smello failure on Fri 16 Jun 2023 11:03:35 AM EDT
+[2023-06-16 11:03:35] **Attention, as of Fri 16 Jun 2023 11:03:35 AM EDT application smello is NOT running and failed to restart.
+Please investigate.
+
+[2023-06-16 11:03:35] *error, no mail service !
+```
--- monstat.sh
+++ monstat.sh
@@ -23,7 +23,7 @@
 set -o pipefail
 
 . ~/.bashrc
-VERSION="0.0.1"
+VERSION="1.0.1"
 is_test=false
 is_background_process=false
 APP=$(basename -s .sh $0)
@@ -53,12 +53,15 @@ usage()
     cat << EOFU!
  Usage: $0 [flags]
 
-Starts and monitors status of arbitrary scripts.
+Starts and monitors status of arbitrary scripts, either as daemon(s)
+or shorter running applications.
 
 Flags:
--b, -bg, --bg: Put process in background.
+-c, -commands, --commands[/foo/bar]: Required. Path of commands file.
+-d, -daemon, --daemon: Long running scripts to be run in the background.
+-e, -email, --email="user@example.com customer@example.com": Specifies the email addressees.
 -h, -help, --help: This help message.
--r, -retry, --retry[int]: Retry to start the script 'n' times before mailing.
+-r, -retry, --retry[int]: Retry to start the script 'n' times before mailing. Default $RETRIES.
 -t, -test, --test: Display debug information to STDOUT.
 -v, -version, --version: Print monstat.sh version and exits.
  Example:
@@ -71,9 +74,9 @@ EOFU!
 message_staff()
 {
     local app="$1"
-    local email_addresses="$1"
+    local addresses="$2"
     local readable_date=$(date)
-    local warning_message="**Attention, on $readable_date application $app is NOT running!\nand failed to restart. Please investigate.\n"
+    local warning_message="**Attention, as of $readable_date application $app is NOT running and failed to restart.\nPlease investigate.\n"
     local today=$(date +'%Y%m%d')
     local notified="$MONSTAT_DIR/${app}.notified"
     local mailer=$(which "$MAIL_SERVICE")
@@ -81,11 +84,12 @@ message_staff()
     local last_notified=$(tail -n 1 "$notified")
     if [ "$last_notified" == "" ] || [ "$last_notified" -ne "$today" ]; then
         # Email addressees
-        logit "$email_addresses emailed about $app failure on $readable_date"
+        logit "$addresses emailed about $app failure on $readable_date"
         if [ -z "$mailer" ] || [ ! -f "$mailer" ]; then
+            logit "$warning_message"
             logit "*error, no mail service $mailer!"
         else
-            echo -e "$warning_message" | $mailer -s"** Process $app failed on $readable_date **" "$email_addresses"
+            echo -e "$warning_message" | $mailer -s"** Process $app failed on $readable_date **" "$addresses"
         fi
         # Save the date so we don't spam until and only if the application is still not running tomorrow.
         echo "$today" >>"$notified"
@@ -98,7 +102,7 @@ message_staff()
 # -l is for long options with double dash like --version
 # the comma separates different long options
 # -a is for long options with single dash like -version
-options=$(getopt -l "bg,help,retry:,test,version" -o "bhr:tv" -a -- "$@")
+options=$(getopt -l "commands:,daemon,email:,help,retry:,test,version" -o "c:de:hr:tv" -a -- "$@")
 if [ $? != 0 ] ; then echo "Failed to parse options...exiting." >&2 ; exit 1 ; fi
 # set --:
 # If no arguments follow this option, then the positional parameters are unset. Otherwise, the positional parameters
@@ -108,8 +112,16 @@ eval set -- "$options"
 while true
 do
     case $1 in
-    -b|--bg)
+    -c|--commands)
+        shift
+        COMMAND_FILE="$1"
+        ;;
+    -d|--daemon)
         is_background_process=true
+        ;;
+    -e|--email)
+        shift
+        EMAIL_ADDRESSES="$1"
         ;;
     -h|--help)
         usage
@@ -118,10 +130,6 @@ do
     -r|--retry)
         shift
         RETRIES=$(echo "$1 + 0" | bc -l)
-        # Find the tries file and see what the last count was.
-        if [ ! -f "$TRIES_FILE" ]; then
-            echo "0" > "$TRIES_FILE"
-        fi
         ;;
     -t|--test)
         is_test=true
@@ -137,41 +145,57 @@ do
     esac
     shift
 done
-
+: "${COMMAND_FILE:?Missing -c, --commands file parameter.}" "${EMAIL_ADDRESSES:?Missing -e,--email parameter.}"
 
 # The command to run should be the first non-commented line is the command file.
 confirm_running_or_start()
 {
-    COMMAND_FILE="$MONSTAT_DIR/commands"
     local tries_so_far=''
-    tries_so_far=$(cat "$TRIES_FILE")
-    tries_so_far=$(echo "$tries_so_far + 1" | bc -l)
+    local my_tries_file=''
     if [ -f "$COMMAND_FILE" ]; then
         while read -r line; do
             # Ignore lines that confirm_running_or_start with comments.
             [ -z "$(echo $line | egrep -ve '^#')" ] && continue
+            # Get the name of script that is running - just the name.
+            # Works if second parameter is a .js or .sh file, and if not just use the first param. Adjust to suit.
+            local my_app=$(basename "$line" | awk '{if ($2 ~ /(.js|.sh)/) {print $2} else {print $1}}')
+            my_tries_file="$my_app.tries"
+            if [ ! -f "$my_tries_file" ]; then
+                echo "0" > "$my_tries_file"
+            fi
+            tries_so_far=$(cat "$my_tries_file")
+            tries_so_far=$(echo "$tries_so_far + 1" | bc -l)
             # Test if the script is already running.
             result=$(ps aux | grep "$line" | grep -v "grep")
             if [ -z "$result" ]; then
                 # Not running so test how many starting attempts have been made.
-                if (("$tries_so_far" > "$RETRIES")); then 
-                    # email and exit
-                    message_staff "$APP" "$EMAIL_ADDRESSES"
-                    exit 1
-                else
-                    echo "$tries_so_far" > "$TRIES_FILE"
-                    if (("$tries_so_far" < 1)); then 
-                        logit "*warning, failed to start '$line' on attempt no. $tries_so_far"
+                if [ "$is_background_process" == true ]; then
+                    if (("$tries_so_far" > "$RETRIES")); then 
+                        # email and continue with any more processes.
+                        logit "**error, $my_app daemon failed to start after $RETRIES attempts"
+                        message_staff "$my_app" "$EMAIL_ADDRESSES"
+                        continue
+                    fi
+                    echo "$tries_so_far" > "$my_tries_file"
+                    if (("$tries_so_far" > 1)); then 
+                        logit "*warning, attempt # $tries_so_far to start $line as daemon"
+                    else
+                        logit "Starting '$line' daemon, attempt no. $tries_so_far"
                     fi
                     # continue to start the file again
-                fi
-                if [ "$is_background_process" == true ]; then
-                    logit "attempting no. $tries_so_far to start '$line' in background "
                     command $line >>"$LOG_FILE"&
                 else
+                    # Start the non-daemon process.
                     result=$(command $line)
+                    # Only update tries if the command is not a daemon and failed to run.
                     if (("$?" > 0)); then
                         logit "command '$line' failed."
+                        echo "$tries_so_far" > "$my_tries_file"
+                        if (("$tries_so_far" > "$RETRIES")); then 
+                            # email
+                            logit "**error, $my_app failed to run after $RETRIES attempts"
+                            message_staff "$my_app" "$EMAIL_ADDRESSES"
+                        fi
                         continue
                     fi
                     if [ -z "$result" ]; then
@@ -179,11 +203,12 @@ confirm_running_or_start()
                     else
                         logit "command '$line' output:\n$result"
                     fi
+                    echo "0" > "$my_tries_file"
                 fi
             else # Script is running.
                 [ "$is_test" == true ] && logit "$line is already running."
                 # Reset the number of tries to start the file.
-                echo "0" > "$TRIES_FILE"
+                echo "0" > "$my_tries_file"
             fi
         done <"$COMMAND_FILE"
     else
